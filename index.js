74ebec4c07010bb8455dd041fcbc4f4d109c5601
Beecer <94152383+Beecer@users.noreply.github.com>
2022-01-26 18:09:26 -0800

    Updated index.js with express code

--- index.js
+++ index.js
@@ -1,72 +1,243 @@
 const express = require("express"),
-  morgan = require("morgan");
+  bodyParser = require("body-parser"),
+  uuid = require("uuid");
 
 const app = express();
 
-let Movies = [
+app.use(bodyParser.json());
+
+let users = [
   {
-    title: "Top Gun",
-    director: "Tony Scott"
+    id: 1,
+    name: "BC",
+    favoriteMovies: []
   },
   {
-    title: "The Sandlot",
-    director: "David Mickey Adams"
-  },
-  {
-    title: "Rocky IV",
-    director: "Sylevester Stallone"
-  },
-  {
-    title: "The Shawshank Redemption",
-    director: "Frank Darabont"
-  },
-  {
-    title: "Forrest Gump",
-    director: "Robert Zemeckis"
-  },
-  {
-    title: "300",
-    director: "Zack Snyder"
-  },
-  {
-    title: "Wedding Crashers",
-    director: "David Dobkin"
-  },
-  {
-    title: "Friday",
-    director: "F. Gary Gray"
-  },
-  {
-    title: "Stepbrothers",
-    director: "Adam McKay"
-  },
-  {
-    title: "Legends of the Fall",
-    director: "Edward Zwick"
+    id: 2,
+    name: "Kristin",
+    favoriteMovies: ["Wedding Crashers"]
   }
 ];
 
-// GET requests
+let movies = [
+  {
+    Title: "Top Gun",
+    Director: {
+      Name: "Tony Scott",
+      About: "NA"
+    },
+    Genre: {
+      Name: "Action"
+    }
+  },
+  {
+    Title: "The Sandlot",
+    Director: {
+      Name: "David Mickey Adams",
+      About: "NA"
+    },
+    Genre: {
+      Name: "Comedy"
+    }
+  },
+  {
+    Title: "Rocky IV",
+    Director: {
+      Name: "Sylevester Stallone",
+      About: "NA"
+    },
+    Genre: {
+      Name: "Action"
+    }
+  },
+  {
+    Title: "The Shawshank Redemption",
+    Director: {
+      Name: "Frank Darabont",
+      About: "NA"
+    },
+    Genre: {
+      Name: "Drama"
+    }
+  },
+  {
+    Title: "Forrest Gump",
+    Director: {
+      Name: "Robert Zemeckis",
+      About: "NA"
+    },
+    Genre: {
+      Name: "Drama"
+    }
+  },
+  {
+    Title: "300",
+    Director: {
+      Name: "Zack Snyder",
+      About: "NA"
+    },
+    Genre: {
+      Name: "Action"
+    }
+  },
+  {
+    Title: "Wedding Crashers",
+    Director: {
+      Name: "David Dobkin",
+      About: "NA"
+    },
+    Genre: {
+      Name: "Comedy"
+    }
+  },
+  {
+    Title: "Friday",
+    Director: {
+      Name: "F. Gary Gray",
+      About: "NA"
+    },
+    Genre: {
+      Name: "Comedy"
+    }
+  },
+  {
+    Title: "Stepbrothers",
+    Director: {
+      Name: "Adam McKay",
+      About: "NA"
+    },
+    Genre: {
+      Name: "Comedy"
+    }
+  },
+  {
+    Title: "Legends of the Fall",
+    Director: {
+      Name: "Edward Zwick",
+      About: "NA"
+    },
+    Genre: {
+      Name: "Drama"
+    }
+  }
+];
+
+//Create
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("users need names");
+  }
+});
+
+// READ requests
 app.get("/movies", (req, res) => {
-  res.send(movies);
-});
-app.get("/", (req, res) => {
-  res.send("These are my favorite movies!");
-});
-app.use(express.static("public"));
-
-app.use(morgan("common"));
-
-app.get("/secreturl", (req, res) => {
-  res.send("Shhhh, this is a secret!");
+  res.status(200).json(movies);
 });
 
-app.use((err, req, res, next) => {
-  console.err(err.stack);
-  res.status(500).send("Oops, something broke!");
+//READ movies title
+app.get("/movies/:Title", (req, res) => {
+  const { title } = req.params;
+  const movie = movies.find(movie => movie.title === title);
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send("We don't have that one");
+  }
 });
 
-// listen for requests
+//READ movies genre
+app.get("/movies/genre/:genreName", (req, res) => {
+  const { genreName } = req.params;
+  const genre = movies.find(movie => movie.Genre.Name === genreName).Genre;
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send("We don't have that one");
+  }
+});
+
+//READ director about
+app.get("/movies/director/:directorName", (req, res) => {
+  const { directorName } = req.params;
+  const director = movies.find(movie => movie.Director.Name === directorName)
+    .Director;
+
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send("We don't have that one");
+  }
+});
+
+//UPDATE user info
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find(user => user.id == id);
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+//CREATE add movie to users list
+app.post("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find(user => user.id == id);
+
+  if (user) {
+    user.favoriteMovies.push(movieTitle);
+    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+//DELETE movie from users list
+app.delete("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find(user => user.id == id);
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter(
+      title => title !== movieTitle
+    );
+    res
+      .status(200)
+      .send(`${movieTitle} has been removed from user ${id}'s array`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+//DELETE deregister user
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find(user => user.id == id);
+
+  if (user) {
+    users = users.filter(user => user.id != id);
+    res.status(200).send(`user ${id} has been deleted`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
 app.listen(8080, () => {
-  console.log("Your app is listening on port 8080.");
+  console.log("Your app is listening on ports 8080");
 });
