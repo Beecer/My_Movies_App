fd3d056a5f8b110b89ff860fa355e31c4d7fd33d
Beecer <94152383+Beecer@users.noreply.github.com>
2022-03-03 16:16:01 -0800

    muted passport.athenticate on app.get('/movies')

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ app.get(
 // READ list of movies // Success
 app.get(
   "/movies",
-  passport.authenticate("jwt", { session: false }),
+  /*passport.authenticate("jwt", { session: false }),*/
   (req, res) => {
     Movies.find()
       .then(movies => {
